a88cb78ba57f96f86db38453e9f34082540c5fa2
Lyubenov <m.liubenow@gmail.com>
2018-02-02 17:30:37 +0200

    Push to trigger deploy in Heroku

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 
 var org = nforce.createConnection({
-  clientId: '3MVG9uudbyLbNPZMk0vYn7ICarLW4qV5bLdL.KqYws.i1.oN99y14Skth6utXg0nwCuPpSMtr9lB7HIOx6M65',
-  clientSecret: '6326007125179395206',
-  redirectUri: 'https://protected-fortress-46904.herokuapp.com/oauth/_callback',
-  apiVersion: 'v34.0',  // optional, defaults to current salesforce API version
-  environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
-  mode: 'multi' // optional, 'single' or 'multi' user mode, multi default
+    clientId: '555555',
+    clientSecret: '666666',
+    redirectUri: 'https://protected-fortress-46904.herokuapp.com/oauth/_callback',
+    apiVersion: 'v34.0',  // optional, defaults to current salesforce API version
+    environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+    mode: 'multi' // optional, 'single' or 'multi' user mode, multi default
 });
 
 var app = express();
@@ -23,21 +23,21 @@ app.use('/home', routesHome);
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req,res){
-  res.redirect(org.getAuthUri());
+app.get('/', function (req, res) {
+    res.redirect(org.getAuthUri());
 });
 
-app.get('/oauth/_callback', function(req, res) {
-  org.authenticate({code: req.query.code}, function(err, resp){
-    if(!err) {
-      console.log('Access Token: ' + resp.access_token);
-      app.locals.oauthtoken = resp.access_token;
-      app.locals.lightningEndPointURI = "https://sedreambmo-dev-ed.lightning.force.com";
-      res.redirect('/home');
-    } else {
-      console.log('Error: ' + err.message);
-    }
-  });
+app.get('/oauth/_callback', function (req, res) {
+    org.authenticate({code: req.query.code}, function (err, resp) {
+        if (!err) {
+            console.log('Access Token: ' + resp.access_token);
+            app.locals.oauthtoken = resp.access_token;
+            app.locals.lightningEndPointURI = "https://bitsaboutme-dev-ed.lightning.force.com";
+            res.redirect('/home');
+        } else {
+            console.log('Error: ' + err.message);
+        }
+    });
 });
 
 // Served Localhost
